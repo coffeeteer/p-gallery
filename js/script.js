e6d464a4cdf4d980b94a9be49aeb9687d2f72f3d
Coffeeteer <tgrieshiemer@gmail.com>
2016-11-28 18:47:18 -0500

    basic logic for js, needs debugging

--- js/script.js
+++ js/script.js
@@ -2,10 +2,28 @@ $(document).ready(function() {
 	$('nav a').on('click', function() {
 
 		//Current Class Assignment
-		$('nav a').removeClass('current');
+		$('nav li.current').removeClass('current');
 		$(this).parent().addClass('current');
 
 		//Set Heading Text
-		$('h1.heading').text($(this).text());
+		$('h1#heading').text($(this).text());
+
+		//Get & Filter link text
+		var category = $(this).text().toLowerCase().replace(' ', '-');
+
+		//Remove hidden class if 'all-projects' is selected
+		if(category == 'all-projects') {
+			$('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+		} else {
+			$('ul#gallery li').each(function(){
+				if(!$(this).hasClass(category)) {
+					$(this).hide().addClass('hidden')
+				} else {
+					$(this).fadeIn('slow').removeClass('hidden');
+				}
+			});
+		}
+		//Stop Link Behavior
+		return false;
 	});
 });
